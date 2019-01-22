6a42827069e7ea55692748f85c9f74bac2460d3a
Alonso Ortega <alonsojortega@gmail.com>
2019-01-22 22:43:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,18 @@ def countdown(n)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(n)
+  counter = n
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    sleep(1)
+    n -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+
+
+
+
